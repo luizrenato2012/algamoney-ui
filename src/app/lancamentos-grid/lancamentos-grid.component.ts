7f81ad3fdd774355bb83424da64d438453ae145c
Luiz Renato <luizrenato2005@gmail.com>
2019-07-22 00:13:27 -0300

    criado componente de grid de lancamentos

--- src/app/lancamentos-grid/lancamentos-grid.component.ts
+++ src/app/lancamentos-grid/lancamentos-grid.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-lancamentos-grid',
   templateUrl: './lancamentos-grid.component.html',
   styleUrls: ['./lancamentos-grid.component.css']
 })
-export class LancamentosGridComponent implements OnInit {
+export class LancamentosGridComponent {
 
-  constructor() { }
+  @Input()
+  private lancamentos = [];
 
-  ngOnInit() {
-  }
+  constructor() {
+    console.log("LancamentosGridComponent criado");
+    console.log(this.lancamentos);
+   }
 
 }
