56ca921e84cbb4a235a2c52be66e671552be1f3a
Luiz Renato <luizrenato2005@gmail.com>
2019-07-22 16:52:17 -0300

    criado módule core para componente navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { NavbarComponent } from './navbar/navbar.component';
-
 import { AppComponent } from './app.component';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { PessoasModule } from './pessoas/pessoas.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     LancamentosModule,
-    PessoasModule
+    PessoasModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
